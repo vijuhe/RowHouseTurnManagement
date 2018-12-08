cdd79875a52b4b3d10e7a50f75d913897827bd88
Ville Henriksson <ville.henriksson@vincit.fi>
2018-12-08 11:08:14 +0200

    Add street address API regex validation and fix postal code validation.

--- UI/UI.MobileAppService/Models/Apartment.cs
+++ UI/UI.MobileAppService/Models/Apartment.cs
@@ -8,10 +8,11 @@ namespace UI.MobileAppService.Models
         public string LastName { get; set; }
 
         [Required]
+        [RegularExpression(@"\d+\s*$", ErrorMessage = "Street address must end with numbers.")]
         public string StreetAddress { get; set; }
 
         [Required]
-        [RegularExpression(@"\d{5}", ErrorMessage = "Postal code must contain five numbers.")]
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "Postal code must consist of five numbers.")]
         public int PostalCode { get; set; }
     }
 }
